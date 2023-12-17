2e8a2391d473d964e781a3a290a99adbffbfec91
Michael Tuttle <tuttlem@gmail.com>
2023-12-18 08:05:17 +1000

    Initial commit

---
+++ main.c
@@ -0,0 +1,45 @@
+#include <string.h>
+#include <unistd.h>
+
+#include "ced.h"
+
+/**
+ * @brief Handler function for client connections of the echo server
+ * @param client The client to handle
+ */
+void echo_client_handler(ced_server_client_p client) {
+    ced_log("Currently managing %d clients\n", client->server->clients->size);
+    ced_log("Client %d: %s:%d\n", client->fd, client->host, client->port);
+
+    ced_buffer_p buffer = ced_buffer_new_handle(client->fd, 0);
+
+    if (buffer != NULL) {
+        if (ced_buffer_cmp_str(buffer, "exit") == 0) {
+            client->server->running = 0;
+        } else {
+            write(client->fd, buffer->data, buffer->size);
+        }
+
+        ced_buffer_free(buffer);
+    }
+
+    // all done with the client
+    ced_server_client_finish(client);
+}
+
+/**
+ * @brief Entry point for the echo server
+ * @return 0 on success, 1 on failure
+ */
+int main() {
+    ced_server_p server = ced_server_new(NULL, 3001, echo_client_handler);
+
+    if (ced_server_start_mio(server) != CED_SUCCESS) {
+        return EXIT_FAILURE;
+    }
+
+    ced_server_stop(server);
+    ced_server_free(server);
+
+    return EXIT_SUCCESS;
+}
